3d32a008059af46fecdb559906495e056b1f404b
essayeswajih <wajihsayes@gmail.com>
2025-08-08 15:39:39 +0100

    Add: contact update

--- src/app/about/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function AboutPage() {
-  return (
-    <main style={{ padding: "2rem" }}>
-      <h1>About Us</h1>
-      <p>Welcome to the About page. This app is built with Next.js 13+ and TypeScript!</p>
-      
-    </main>
-  );
-}
